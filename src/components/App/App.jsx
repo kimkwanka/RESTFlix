6075c9b2e676139f95da2bd1a28a244775633ab6
kimkwanka <kimkwanka@gmail.com>
2021-07-01 09:19:12 +0200

    refactor: move App component to its own file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import MainView from '../MainView';
+
+const App = () => (
+  <MainView />
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.scss';
-
-import MainView from './components/MainView';
-
-const App = () => (
-  <MainView />
-);
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
