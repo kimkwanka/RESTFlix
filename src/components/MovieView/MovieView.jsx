26ec372a35e2fb57f6f2065d408dafa8b3d3bfb2
kimkwanka <kimkwanka@gmail.com>
2021-07-13 13:31:46 +0200

    feat: add Link to Genre and Director on MovieView

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 
 import Button from 'react-bootstrap/Button';
@@ -13,6 +15,14 @@ const MovieView = ({ movie, onBackClick }) => (
     <Card.Img crossOrigin="anonymous" variant="top" src={`${imgRoot}${movie.ImagePath}`} />
     <Card.Body>
       <Card.Title>{movie.Title}</Card.Title>
+      <p>
+        Genre:&nbsp;
+        <Link to={`/genres/${movie.Genre.Name}`}>{movie.Genre.Name}</Link>
+      </p>
+      <p>
+        Director:&nbsp;
+        <Link to={`/directors/${movie.Director.Name}`}>{movie.Director.Name}</Link>
+      </p>
       <Card.Text>{movie.Description}</Card.Text>
     </Card.Body>
     <Button className="m-4" onClick={onBackClick}>Back</Button>
@@ -24,6 +34,8 @@ MovieView.propTypes = {
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
+    Director: PropTypes.shape.isRequired,
+    Genre: PropTypes.shape.isRequired,
   }),
   onBackClick: PropTypes.func.isRequired,
 };
@@ -33,6 +45,12 @@ MovieView.defaultProps = {
     Title: '',
     Description: '',
     ImagePath: '',
+    Director: {
+      Name: '',
+    },
+    Genre: {
+      Name: '',
+    },
   },
 };
 
