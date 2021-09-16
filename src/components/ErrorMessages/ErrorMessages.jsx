26b213e2468f51b55bdbc289716441b2786be609
Kim Kwanka <kimkwanka@gmail.com>
2021-09-16 09:43:00 +0200

    refactor: replace redux connect with hooks

--- src/components/ErrorMessages/ErrorMessages.jsx
+++ src/components/ErrorMessages/ErrorMessages.jsx
@@ -1,23 +1,26 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import './ErrorMessages.scss';
 
-const ErrorMessages = ({ errorMessages }) => (
-  <div className="m-4">
-    <div>
-      {errorMessages.map((e, i) => <p className="errorText" key={`err${i}`}>{e}</p>)}
-    </div>
-  </div>
-);
+const selectErrorMessages = (state) => state.errorMessages;
 
-ErrorMessages.propTypes = {
-  errorMessages: PropTypes.arrayOf(PropTypes.shape).isRequired,
+const ErrorMessages = () => {
+  const errorMessages = useSelector(selectErrorMessages);
+
+  return (
+    <div className="m-4">
+      <div>
+        {errorMessages.map((e, i) => (
+          <p className="errorText" key={`err${i}`}>
+            {e}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default connect((store) => ({
-  errorMessages: store.errorMessages,
-}))(ErrorMessages);
+export default ErrorMessages;
