99b8fe53ca0ec347534ae0a93284044dea2ea7f9
Kim Kwanka <kimkwanka@gmail.com>
2021-09-16 17:47:02 +0200

    feat: add basic nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,37 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import { Link } from 'react-router-dom';
+
 import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
 
 import Routes from './Routes';
 
 const App = () => {
   const isLoading = useSelector((state) => state.isLoading);
+  const loggedInUser = useSelector((state) => state.user);
+
   return (
     <>
+      {loggedInUser ? (
+        <header>
+          <nav>
+            <h1 className="logo">myFlix</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/logout">Logout</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      ) : null}
+
       <LoadingSpinner isLoading={isLoading} />
       {Routes}
     </>
