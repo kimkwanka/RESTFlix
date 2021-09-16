3a7c43f95e55853abbf2c5ef3c36603448b0a467
Kim Kwanka <kimkwanka@gmail.com>
2021-09-16 07:48:19 +0200

    feat: add ProfileView route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import {
-  Switch, Route,
-} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import LoginView from './views/LoginView/LoginView';
 import HomeView from './views/HomeView/HomeView';
 import MovieView from './views/MovieView/MovieView';
 import GenreView from './views/GenreView/GenreView';
 import DirectorView from './views/DirectorView/DirectorView';
+import ProfileView from './views/ProfileView/ProfileView';
 
 const Routes = (
   <Switch>
@@ -16,6 +15,7 @@ const Routes = (
     <Route exact path="/movies/:movieID" component={MovieView} />
     <Route exact path="/genres/:genreName" component={GenreView} />
     <Route exact path="/directors/:directorName" component={DirectorView} />
+    <Route exact path="/profile" component={ProfileView} />
   </Switch>
 );
 
