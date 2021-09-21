d27fe0f044116ce42219cd9ec9cc04d3f2a8b42e
Kim Kwanka <kimkwanka@gmail.com>
2021-09-21 13:34:41 +0200

    fix: remove duplicate redirect

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 
-import { Redirect } from 'react-router-dom';
-
-import { useSelector } from 'react-redux';
-
 import MoviesList from './MoviesList/MoviesList';
 
-const HomeView = () => {
-  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-
-  return (
-    isLoggedIn
-      ? <MoviesList />
-      : <Redirect to="/login" />
-  );
-};
+const HomeView = () => (
+  <MoviesList />
+);
 
 export default HomeView;
