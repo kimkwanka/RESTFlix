983da4ea53bc416a280883063dbf596438a40212
kimkwanka <kimkwanka@gmail.com>
2021-09-24 11:25:16 +0200

    feat: make SearchBar animated and toggleable

--- src/components/NavBar/SearchBar/SearchBar.jsx
+++ src/components/NavBar/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -11,15 +11,32 @@ const SearchBar = () => {
   const searchTerm = useSelector((state) => state.ui.searchTerm);
   const dispatch = useDispatch();
 
+  const inputRef = useRef(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenClick = () => {
+    setIsOpen(true);
+    inputRef.current.focus();
+  };
+
+  const handleLoseFocus = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="search-bar">
-      <span className="search-bar__icon">&#8981;</span>
+    <div className={`search-bar ${isOpen ? 'search-bar--opened' : ''}`}>
+      <button type="button" className="search-bar__open-button" onClick={handleOpenClick}>
+        <span className="search-bar__open-button__icon">&#8981;</span>
+      </button>
       <input
         type="text"
         className="search-bar__input"
         onChange={(e) => dispatch(setSearchTerm(e.target.value))}
         value={searchTerm}
         placeholder="Search myFlix"
+        onBlur={handleLoseFocus}
+        ref={inputRef}
       />
     </div>
 
