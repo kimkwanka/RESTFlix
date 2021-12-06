6d32418c45c30516f5d896eaa6bd87ccd40dc997
kimkwanka <kimkwanka@gmail.com>
2021-12-06 13:37:46 +0100

    feat: add proper pagination to HomeView

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,8 +1,57 @@
-import { useAppSelector } from '@features/hooks';
+import { useState, useRef, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
-import { TmdbMovieSimple } from '@features/types';
+import { useDiscoverMoviesQuery } from '@features/slices/api';
 
-import FilteredMoviesList from '@components/FilteredMoviesList/FilteredMoviesList';
+import MovieCard from '@components/MovieCard/MovieCard';
+
+import './HomeView.scss';
+
+interface IPaginationProps {
+  initialPage: number;
+  totalPages: number;
+}
+
+const Pagination = ({ initialPage, totalPages }: IPaginationProps) => {
+  const history = useHistory();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
+  useEffect(() => {
+    setCurrentPage(initialPage);
+  }, [initialPage]);
+
+  return (
+    <div className="home-pagination-links">
+      {initialPage > 1 && <Link to={`/${initialPage - 1}`}>&lt; Previous</Link>}
+      <input
+        className="home-pagination-input"
+        type="number"
+        value={currentPage}
+        onChange={(e) => {
+          if (inputRef?.current?.reportValidity()) {
+            setCurrentPage(parseInt(e.currentTarget.value, 10));
+          }
+        }}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            if (inputRef?.current?.reportValidity()) {
+              history.push(`/${currentPage}`);
+            }
+          }
+        }}
+        min={1}
+        max={totalPages}
+        required
+        ref={inputRef}
+      />
+      <p>of {totalPages}</p>
+      {initialPage < totalPages && (
+        <Link to={`/${initialPage + 1}`}>Next &gt;</Link>
+      )}
+    </div>
+  );
+};
 
 interface IHomeViewProps {
   match: {
@@ -17,16 +66,26 @@ const HomeView = ({
     params: { page = '1' },
   },
 }: IHomeViewProps) => {
-  const searchTerm = useAppSelector((state) => state.ui.searchTerm);
+  const pageAsNumber = parseInt(page, 10);
 
-  const filterMoviesBySearchTerm = (movie: TmdbMovieSimple) =>
-    movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  const { data } = useDiscoverMoviesQuery(pageAsNumber);
+
+  if (!data) {
+    return null;
+  }
+
+  const { movies, totalPages } = data;
 
   return (
-    <FilteredMoviesList
-      page={parseInt(page, 10)}
-      filterFunc={filterMoviesBySearchTerm}
-    />
+    <>
+      <Pagination initialPage={pageAsNumber} totalPages={totalPages} />
+      <div className="home-movies-list">
+        {movies?.map((movie) => (
+          <MovieCard key={`${movie.id}`} movie={movie} />
+        ))}
+      </div>
+      <Pagination initialPage={pageAsNumber} totalPages={totalPages} />
+    </>
   );
 };
 
