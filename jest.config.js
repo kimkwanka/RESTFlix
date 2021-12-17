5d1a4149d1eb8bced3667c0ca52f9bfeb35da32e
kimkwanka <kimkwanka@gmail.com>
2021-12-17 23:54:02 +0100

    fix: remove extra slash

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '^#components/(.*)$': '<rootDir>//src/components/$1',
+    '^#components/(.*)$': '<rootDir>/src/components/$1',
     '^#redux/(.*)$': '<rootDir>/src/redux/$1',
     '^#hooks/(.*)$': '<rootDir>/src/hooks/$1',
     '^#views/(.*)$': '<rootDir>/src/views/$1',
